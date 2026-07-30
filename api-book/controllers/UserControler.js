c09f579d742cb30858a5bb10b3c5b9e6f4c9f047
Rahulthakur <rahulthakursir65@gmail.com>
2026-07-30 16:43:07 +0530

    Fix Nodemailer Gmail SMTP

--- api-book/controllers/UserControler.js
+++ api-book/controllers/UserControler.js
@@ -8,15 +8,25 @@ const nodemailer = require("nodemailer");
 const jwt = require("jsonwebtoken");
 
 
+// const transporter = nodemailer.createTransport({
+//     service: "gmail",
+//     auth: {
+//         user: process.env.EMAIL_USER,
+//         pass: process.env.EMAIL_PASS
+//     },
+//     connectionTimeout: 60000,
+//     greetingTimeout: 60000,
+//     socketTimeout: 60000
+// });
+
 const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
-    },
-    connectionTimeout: 60000,
-    greetingTimeout: 60000,
-    socketTimeout: 60000
+    }
 });
 
 
